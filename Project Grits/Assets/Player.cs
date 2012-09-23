24b8c851348c631733c4b08ef455c794b58a2002
Corey Thomas <led2rhyme@yahoo.com>
2012-09-23 00:03:05 -0400

    Updated git ignore file, fix compile errors, added new files
    Signed-off-by: Corey Thomas <led2rhyme@yahoo.com>

---
+++ Project Grits/Assets/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : CharacterBasics {
+	
+    #region Mono Inherit Functions
+	
+	public void Update()
+	{
+        
+
+
+        BaseMovement(InputMovement(), InputMovement().magnitude );
+		if (Input.GetKeyDown(KeyCode.Space)  || Input.GetKeyDown(KeyCode.JoystickButton1))
+		{
+			base.Launch();
+		}
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            manager.SendMessage("Switch");
+        }
+	}
+
+    #endregion
+
+    #region Utilities
+
+    /// <summary>
+    /// Recieves the input from the axes
+    /// </summary>
+    /// <returns>
+    /// The movement.
+    /// </returns>
+    private Vector2 InputMovement()
+    {
+        return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    #endregion
+
+
+}
